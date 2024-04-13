281aa516eac0f385b38cc7c16fd409ed2d809672
u220110 <ryan.smith.2022@mumail.ie>
2024-04-13 14:46:40 +0100

    button changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
+from tkinter import ttk #button styles
 from settings import showSettings
-#from settings import goBack
 from settings_frame import SettingsFrame
 from gravity import GravityFrame
 def main():
@@ -12,12 +12,16 @@ def main():
     home_frame = tk.Frame(root)
     gravity_frame = GravityFrame(root,precision)
     settings_frame = SettingsFrame(root,precision)
+    #styles
+    button_style = ttk.Style()
+    button_style.configure("Custom.TButton", background="blue", foreground="blue", font=("Helvetica", 12), padding=10)
 
+    #home page
     home_label = tk.Label(home_frame, text="Home page")
     home_label.grid(row=0, column=0)
-    button_gravity = tk.Button(home_frame, text="Gravity calculator", command=lambda: showSettings(home_frame,gravity_frame))
+    button_gravity = ttk.Button(home_frame,style="Custom.TButton", text="Gravity calculator", command=lambda: showSettings(home_frame,gravity_frame))
     button_gravity.grid(row=1, columnspan=2)
-    button_precision = tk.Button(home_frame, text="Precision", command=lambda: showSettings(home_frame,settings_frame)) #switch to precision on click
+    button_precision = ttk.Button(home_frame,style="Custom.TButton", text="Precision", command=lambda: showSettings(home_frame,settings_frame)) #switch to precision on click
     button_precision.grid(row=2, columnspan=2)
     #***********************************************
     
